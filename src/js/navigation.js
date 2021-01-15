d745f301b1afe4384d93152f43d8f46c1639d88a
Tomasz Bujnicki <tomek.bujnicki@gmail.com>
2021-01-16 00:21:54 +0100

    feat. keaboard menu navigation

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -30,23 +30,35 @@ export function navigation() {
     .addEventListener('click', () => action('fullscreen'));
 
   // focus order
-  const menuButtons = document.querySelectorAll('#menu button');
+  document.querySelector('#menu button').focus();
   const optionsButtons = document.querySelectorAll('#options button');
-  menuButtons[1].focus();
 
   document.addEventListener('keydown', press);
   function press(e) {
-    // F
-    if (e.keyCode === 70) {
-      if (document.activeElement.tagName !== 'INPUT') {
+    if (document.activeElement.tagName !== 'INPUT') {
+      // F
+      if (e.keyCode === 70) {
         action('fullscreen');
       }
-    }
-
-    // M
-    if (e.keyCode === 77) {
-      if (document.activeElement.tagName !== 'INPUT') {
+      // M
+      if (e.keyCode === 77) {
         action('mute');
+      }
+      // Right
+      if (e.keyCode === 39) {
+        if (document.getElementById('playground').offsetWidth === 0) {
+          const firstButton = document.querySelector('.options__button');
+          firstButton.focus();
+        }
+      }
+      // Left
+      if (e.keyCode === 37) {
+        if (document.getElementById('playground').offsetWidth === 0) {
+          const firstButton = document.querySelector(
+            '.game-card:not(.hide) button'
+          );
+          firstButton.focus();
+        }
       }
     }
 
@@ -61,12 +73,40 @@ export function navigation() {
 
     // Down
     if (e.keyCode === 40) {
-      isKeyPressed.space = true;
+      const focused = document.activeElement;
+      const buttons = focused.classList.contains('options__button')
+        ? [...document.querySelectorAll('.options__button')]
+        : [
+            ...document.querySelectorAll(
+              '.game-card:not(.hide) button:not([disabled="true"])'
+            ),
+          ];
+
+      if (buttons.length > 1) {
+        if (focused !== buttons[buttons.length - 1]) {
+          const index = buttons.findIndex((e) => e === focused);
+          buttons[index + 1].focus();
+        }
+      }
     }
 
     // Up
     if (e.keyCode === 38) {
-      isKeyPressed.pause = true;
+      const focused = document.activeElement;
+      const buttons = focused.classList.contains('options__button')
+        ? [...document.querySelectorAll('.options__button')]
+        : [
+            ...document.querySelectorAll(
+              '.game-card:not(.hide) button:not([disabled="true"])'
+            ),
+          ];
+
+      if (buttons.length > 1) {
+        if (focused !== buttons[0]) {
+          const index = buttons.findIndex((e) => e === focused);
+          buttons[index - 1].focus();
+        }
+      }
     }
   }
 }
