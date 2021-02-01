e2dff36b12f12b56d63bb59d1e8b27c5028dce31
Tomasz Bujnicki <tomek.bujnicki@gmail.com>
2021-02-01 03:12:18 +0100

    add initial settings function

--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,22 @@ import { highscore } from './highscore';
 import { theme } from './theme';
 import { keybordControl } from './keybordControl';
 import { Navigation } from './navigation';
+import { SOUND } from './sound';
+import { DOMelements, SVG } from './data';
 
 function init() {
   const game = new Game();
   const navigation = new Navigation(game);
-  highscore.load();
-  theme.get();
+  initialSettings();
   keybordControl();
+}
+
+function initialSettings() {
+  highscore.get();
+  theme.get();
+  SOUND.get();
+  DOMelements.muteBtn.innerHTML = SOUND.isMuted ? SVG.soundOff : SVG.soundOn;
+  DOMelements.volumeInput.value = SOUND.volume;
 }
 
 window.addEventListener('DOMContentLoaded', init);
