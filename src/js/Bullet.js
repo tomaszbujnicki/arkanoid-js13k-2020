efb13ab8cff2859b3823d6f97c30558d1f88ff25
Tomasz Bujnicki <tomek.bujnicki@gmail.com>
2021-02-03 10:58:16 +0100

    change size

--- src/js/Bullet.js
+++ src/js/Bullet.js
@@ -2,16 +2,14 @@ export default class Bullet {
   constructor(paddle) {
     this.id = uniqueId();
     this.type = 'bullet';
-    this.height = 20;
-    this.width = 8;
+    this.height = 36;
+    this.width = 12;
     this.top = paddle.top - this.height;
     this.left = paddle.left + paddle.width / 2 - this.width / 2;
-    this.speedY = 5;
+    this.speedY = 3;
   }
   move() {
-    if (!this.isSticked) {
-      this.top += this.speedY;
-    }
+    this.top -= this.speedY;
   }
 }
 
